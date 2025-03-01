5e61daaeae14ea86ff529e051883434496abff5e
Yan-Ru Liu <61071600+andrew76214@users.noreply.github.com>
2025-03-01 16:43:23 +0800

    Delete utils directory

--- utils/DFS.cpp
+++
@@ -1,8 +0,0 @@
-void dfs(TreeNode* node, vector<int>& leaves){
-  if (node == NULL) return;
-  if (node->left == NULL && node->right == NULL){
-    leaves.push_back(node->val);
-  }
-  dfs(node->left, leaves);
-  dfs(node->right, leaves);
-}
